0b83e8ecafc5b7d5b99e82929e2cea4d7a60eaa3
Zach Barham <me@zoweb.me>
2017-01-30 20:43:16 +1000

    Added class to simplify messages

--- src/me/zoweb/loginmsg/MessagerHelpers.java
+++ src/me/zoweb/loginmsg/MessagerHelpers.java
@@ -1,7 +1,18 @@
 package me.zoweb.loginmsg;
 
+import org.bukkit.entity.Player;
+
 /**
  * LoginMSG (c) zachy 2017
  */
-public class MessagerHelpers {
+class MessagerHelpers {
+
+    static boolean checkPlayerPerms(Player player, String key) {
+        return player.hasPermission(key.replace(' ', '.')) || key.equals("server op") && player.isOp() || key.equalsIgnoreCase("player " + player.getName());
+    }
+
+    static String parsePlayerVariable(String string, Player player) {
+        return string.replace("%player%", player.getName());
+    }
+
 }
