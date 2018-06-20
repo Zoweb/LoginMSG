77bab623344739c5bda193fc175c8e376ecf0957
zoweb <zachymach@live.com>
2018-06-20 14:58:42 +1000

    Add help message ❓

--- src/main/java/me/zoweb/loginmsg/command/LoginMSGCommand.java
+++ src/main/java/me/zoweb/loginmsg/command/LoginMSGCommand.java
@@ -5,6 +5,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
 
 public class LoginMSGCommand implements CommandExecutor {
     public static final String prefix = "&8[&6&LoginMSG&8] &f";
@@ -14,23 +17,37 @@ public class LoginMSGCommand implements CommandExecutor {
 
     public static String generateHelp(String name, String[] options) {
         StringBuilder message = new StringBuilder(prefix);
-        message.append("&").append(name).append("&f command\n");
+        message.append("&o").append(name).append("&f command\n");
 
         for (String current : options) {
-            message.append(" - ").append(current).append("\n");
+            message.append(" - ").append(current).append("\n&f");
         }
 
-        return ChatColor.translateAlternateColorCodes('&', message.substring(0, message.length() - 1));
+        return ChatColor.translateAlternateColorCodes('&', message.substring(0, message.length() - 3));
     }
 
     private LoginMSG plugin;
+
+    private void help(CommandSender sender) {
+        ArrayList<String> options = new ArrayList<>();
+
+        boolean isPlayer = sender instanceof Player;
+
+        options.add("/lmsg reload");
+        options.add("/lmsg <enable|disable> " + (isPlayer ? "[playername]" : "<playername>"));
+        options.add("/lmsg <enable|disable> <login|logout|death> " + (isPlayer ? "[playername]" : "<playername>"));
+
+        sender.sendMessage(generateHelp("LoginMSG", options.toArray(new String[]{})));
+    }
 
     public LoginMSGCommand(LoginMSG plugin) {
         this.plugin = plugin;
     }
 
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        return false;
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 0) help(sender);
+
+        return true;
     }
 }
