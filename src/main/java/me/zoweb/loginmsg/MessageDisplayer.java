ad6efe6534cc246afc533b1dada44cf46bc6adde
zoweb <zachymach@live.com>
2018-06-21 18:03:59 +1000

    Add message displaying system 📠

--- src/main/java/me/zoweb/loginmsg/MessageDisplayer.java
+++ src/main/java/me/zoweb/loginmsg/MessageDisplayer.java
@@ -1,8 +1,14 @@
 package me.zoweb.loginmsg;
 
+import me.zoweb.loginmsg.command.LoginMSGCommand;
 import me.zoweb.loginmsg.lambda.Lambda;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerEvent;
 
@@ -47,6 +53,41 @@ public class MessageDisplayer<TEvent extends PlayerEvent> implements Listener {
     protected Lambda<TEvent> valueResetter;
     public String name;
 
+    private String getValue(Player player, String section) {
+        ConfigurationSection conf = data.getConfigurationSection(section);
+
+        if (!conf.getBoolean("enabled")) return null;
+
+        String[] order = conf.getString("check first").replaceAll("[,;.]", "").split(" then ");
+
+        String matching = readValue(player, conf.getConfigurationSection("when matching"), order, false);
+        if (matching == null) matching = readValue(player, conf.getConfigurationSection("otherwise"), order, true);
+        if (matching == null) matching = conf.getString("default");
+
+        return matching;
+    }
+
+    private String readValue(Player player, ConfigurationSection conf, String[] order, boolean invert) {
+        for (String current : order) {
+            switch (current) {
+                case "ops":
+                    if (player.isOp() ^ invert) return conf.getString("ops");
+                    break;
+                case "usernames":
+                    if (conf.getConfigurationSection("by username").contains(player.getName().replace('.', ' ')) ^ invert)
+                        return conf.getString("by username." + player.getName().replace('.', ' '));
+                    break;
+                case "permissions":
+                    for (String permission : conf.getConfigurationSection("by permission").getKeys(true)) {
+                        String perm = permission.replace(' ', '.');
+                        if (player.hasPermission(perm) ^ invert) return conf.getString("by permission." + permission);
+                    }
+            }
+        }
+
+        return null;
+    }
+
     protected MessageDisplayer(String name, Lambda<TEvent> valueResetter) {
         this.name = name;
         this.valueResetter = valueResetter;
@@ -55,7 +96,7 @@ public class MessageDisplayer<TEvent extends PlayerEvent> implements Listener {
     }
 
     public void loadData(File location) throws IOException, InvalidConfigurationException {
-        this.data.load(location);
+        data.load(location);
     }
 
     public void register() {
@@ -65,6 +106,28 @@ public class MessageDisplayer<TEvent extends PlayerEvent> implements Listener {
     public void onEvent(TEvent event) {
         valueResetter.run(event);
 
+        if (data == null) throw new NullPointerException("Data is null. Run loadData() to fix.");
 
+        Player player = event.getPlayer();
+
+        if (data.getBoolean("messages.enabled")) {
+            String result = getValue(player, "messages");
+            if (result != null) {
+                result = result.replace("%player%", player.getDisplayName());
+                Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', result));
+                for (Player pl : Bukkit.getServer().getOnlinePlayers()) {
+                    pl.sendMessage(ChatColor.translateAlternateColorCodes('&', result));
+                }
+            }
+        }
+        if (data.getBoolean("sounds.enabled")) {
+            String result = getValue(player, "sounds");
+            if (result != null) {
+                Bukkit.getServer().getConsoleSender().sendMessage(LoginMSGCommand.prefix + "(sound: " + result + ")");
+                for (Player pl : Bukkit.getServer().getOnlinePlayers()) {
+                    pl.playSound(pl.getLocation(), result, 1f, 1f);
+                }
+            }
+        }
     }
 }
